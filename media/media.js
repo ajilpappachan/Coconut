74f42e347fe2827d9cc31a26ba4cb499300e954c
Ajil Pappachan <ajilpappachan@gmail.com>
2022-02-10 22:45:42 +0530

    Updated max search results on cloudinary

--- media/media.js
+++ media/media.js
@@ -11,8 +11,10 @@ const media = {
 	getAllMedia: async () => {
 		const result = await cloudinary.v2.search
 			.expression("folder:Coconut/*")
+			.max_results(500)
 			.sort_by("created_at", "desc")
 			.execute();
+		console.log(result.resources);
 		return result.resources;
 	},
 };
